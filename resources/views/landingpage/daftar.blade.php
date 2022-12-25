96533a6ae1cbfe015540059a70f16dcbe42cfcc9
AsepSahrudin18 <asepsahrudin0399@gmail.com>
2022-12-25 17:21:38 +0700

    bug fix anchor

--- resources/views/landingpage/daftar.blade.php
+++ resources/views/landingpage/daftar.blade.php
@@ -4,13 +4,9 @@
     <div class="tittle-header d-flex flex-column justify-content-center align-items-center">
         <h3 class="text-center">Daftar</h3>
         <img class="img-brand" src="assets/image/brand-image(2).png">
-        <a type="button" href="register" class="btn btn-custom-daftar mb-3"> Daftar Sebagai Penghuni Kos</a>
-        <a type="button" href="register-pemilik" class="btn btn-custom-daftar mb-3"> Daftar Sebagai Pemilik Kos</a>
-        <a class="forget text-center" href="login">Sudah Punya Akun</a>
+        <a type="button" href="{{url('register')}}" class="btn btn-custom-daftar mb-3"> Daftar Sebagai Penghuni Kos</a>
+        <a type="button" href="{{url('register-pemilik')}}" class="btn btn-custom-daftar mb-3"> Daftar Sebagai Pemilik Kos</a>
+        <a class="forget text-center" href="{{url('login')}}">Sudah Punya Akun</a>
     </div>
-
-
-    <!-- <div class="logo-brand d-flex justify-content-center">
-    </div> -->
 </div>
 @endsection
--- resources/views/landingpage/navigation.blade.php
+++ resources/views/landingpage/navigation.blade.php
@@ -28,8 +28,8 @@
             <div class="ml-auto button-navbar d-flex">
                 <!-- Added -->
                 @guest
-                <a class="btn btn-custom" href="login" type="button">Login</a>
-                <a class="btn daftar" href="daftar" type="button">Daftar</a>
+                <a class="btn btn-custom" href="{{url('login')}}" type="button">Login</a>
+                <a class="btn daftar" href="{{url('daftar')}}" type="button">Daftar</a>
                 @endguest
                 @auth
                 <div class="dropdown">
